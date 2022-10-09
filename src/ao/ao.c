f71302236d76b8b61a7a12b3b1b5c042e9c60a54
Ali Muhammed <uniapi@outlook.com>
2022-10-10 00:28:33 +0300

    Modified [src/ao: ao.h ao.c] - the <ge_case> generator can be run in place of <data>; 	the contract: <casesz> should be zero and <ge_case> should return the size of <aocase> when passed NULL

--- src/ao/ao.c
+++ src/ao/ao.c
@@ -5,6 +5,7 @@
  */
 
 #include <ao/ao.h>
+#include <stdlib.h>
 
 static __thread ao_t _ao;
 
@@ -48,14 +49,22 @@ aop_t ao_assert(const char *descr, void *data, size_t ncase, size_t casesz,
 	aop_t aop = AOP_PASS;
 	_ao.logger.nlog = _ao.logger.skipped = ncase;
 	_ao.logger.ndx = _ao.logger.passed = _ao.logger.failed = 0;
+	if (casesz)
+		_ao.aocase = (char *)data - casesz;
+	else if (!(_ao.aocase = malloc((*(size_t (*)(void *))data)(NULL))))
+		return AOP_SKIP;
 	fstdout("%s\n", descr);
 	while (ncase--) {
-		aop = (*assert_case)(data);
+		aop = (*assert_case)
+		(
+			casesz ? (_ao.aocase = (char *)_ao.aocase + casesz) : (*(void *(*)(void *))data)(_ao.aocase)
+		);
 		if (ao_log(aop, &_ao) == AOP_FAIL)
 			break;
-		data = (char *)data + casesz;
-	}
-	ao_stats(&_ao);
+    }
+    ao_stats(&_ao);
+    if (!casesz && _ao.aocase)
+        free(_ao.aocase);
 	return aop;
 }
 
@@ -70,13 +79,19 @@ aop_t ao_run(ao_t *ao)
 	if (ao_setdefs(ao) != AOP_PASS)
 		return AOP_SKIP;
 	ncase = ao->ncase;
-	ao->aocase = ao->data;
+	if (ao->casesz)
+		ao->aocase = (char *)ao->data - ao->casesz;
+	else if (!(ao->aocase = malloc((*(size_t (*)(void *))ao->data)(NULL))))
+		return AOP_SKIP;
 	fstdout("%s\n", ao->descr);
 	if (ao->setup)
 		if ((*ao->setup)())
 			aop = AOP_SKIP, ncase = 0;
 	while (ncase--) {
-		aop = (*ao->assert_case)(ao->aocase);
+		aop = (*ao->assert_case)
+		(
+			ao->casesz ? (ao->aocase = (char *)ao->aocase + ao->casesz) : (*(void *(*)(void *))ao->data)(ao->aocase)
+		);
 		if (ao->inc == AOINC_AUTO)
 			ao_log(aop, ao);
 		if (aop == AOP_FAIL) {
@@ -87,8 +102,9 @@ aop_t ao_run(ao_t *ao)
 			}
 		}
 		(*at_case_exit)(ao->aocase);
-		ao->aocase = (char *)ao->aocase + ao->casesz;
 	}
 	ao->ao_stats ? (*ao->ao_stats)(ao) : ao_stats(ao);
+	if (!ao->casesz && ao->aocase)
+		free(ao->aocase);
 	return aop;
 }
